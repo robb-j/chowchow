ad96484158a65f8bfc835a9f1bbccd6dc3652c67
Rob Anderson <rob.anderson@ncl.ac.uk>
2019-01-31 12:19:28 +0000

    :white_check_mark: Add unit tests

--- src/ChowChow.ts
+++ src/ChowChow.ts
@@ -1,3 +1,6 @@
+import stoppable from 'stoppable'
+import { createServer } from 'http'
+
 import express, {
   Application,
   Request,
@@ -17,17 +20,19 @@ export type BaseContext = {
   next: NextFunction
 }
 
+/** A chowchow module, used to add functionality for your routes */
 export interface Module {
   app: ChowChow
   checkEnvironment(): void
   setupModule(): void | Promise<void>
   clearModule(): void
-  extendExpress(server: Application): void
+  extendExpress(expressApp: Application): void
   extendEndpointContext(ctx: BaseContext): { [idx: string]: any }
 }
 
 export type ExpressFn = (app: Application) => void
 
+/** An express error handler, useful for improving code readability */
 export type ExpressHandler = (
   error: any,
   req: Request,
@@ -35,43 +40,62 @@ export type ExpressHandler = (
   next: NextFunction
 ) => void
 
+/** A chowchow route */
 export type ChowChowRoute<T> = (ctx: BaseContext & T) => Promise<void> | void
 
+/** A chowchow error handler (it has the chowchow context) */
 export type ErrorHandler<T> = (
   error: any,
   ctx: BaseContext & T
 ) => Promise<void> | void
 
+/** A function to convert chowchow routes to express routes for you */
 export type RouterFn<T> = (
   app: Application,
   r: (route: ChowChowRoute<T>) => RequestHandler
 ) => void
 
-type Logger = (...args: any) => void
-
 const nameOf = (o: object) => o.constructor.name
 
-export class ChowChow<T extends BaseContext = BaseContext> {
-  modules = new Array<Module>()
-  private server = express()
-  private routesToApply = new Array<RouterFn<any>>()
-  private errorHandlers = new Array<ErrorHandler<any>>()
-  private state = ChowChowState.stopped
+/** An interface to expose ChowChow internals for testing purposes */
+export interface ChowChowInternals {
+  state: ChowChowState.stopped
+  modules: Array<Module>
+  httpServer: stoppable.StoppableServer
+  expressApp: express.Express
+  routesToApply: Array<RouterFn<any>>
+  errorHandlers: Array<ErrorHandler<any>>
+}
 
+/** The chowchow app, where everything chowchow starts */
+export class ChowChow<T extends BaseContext = BaseContext> {
+  protected state = ChowChowState.stopped
+  protected modules = new Array<Module>()
+  protected expressApp = express()
+  protected httpServer = stoppable(createServer(this.expressApp))
+  protected routesToApply = new Array<RouterFn<any>>()
+  protected errorHandlers = new Array<ErrorHandler<any>>()
+
+  /** A static function to create a chow, useful for chaining. */
   static create<T extends BaseContext>(): ChowChow<T> {
     return new ChowChow<T>()
   }
 
+  /** Add a module to chowchow, call order is important.
+    Modules callbacks are called in chronological order on setup
+    and reverse chronological order in teardown. */
   use(module: Module): ChowChow<T> {
     module.app = this
     this.modules.push(module)
     return this
   }
 
+  /** Apply middleware to the internal express app. */
   applyMiddleware(fn: ExpressFn) {
-    fn(this.server)
+    fn(this.expressApp)
   }
 
+  /** Generate a chowchow context based on the current modules. */
   makeCtx(req: Request, res: Response, next: NextFunction): any {
     let ctx: any = { req, res, next }
     for (let module of this.modules) {
@@ -80,6 +104,11 @@ export class ChowChow<T extends BaseContext = BaseContext> {
     return ctx
   }
 
+  /**
+    Register chowchow routes with a generator,
+    the generator isn't called until #start is called.
+    It will throw if chowchow is already started.
+  */
   applyRoutes(fn: RouterFn<T>) {
     if (this.state === ChowChowState.running) {
       throw new Error('Cannot add routes once running')
@@ -87,6 +116,11 @@ export class ChowChow<T extends BaseContext = BaseContext> {
     this.routesToApply.push(fn)
   }
 
+  /** 
+    Register chowchow error handlers with a generator,
+    the generator isn't called until #start is called.
+    It will throw if chowchow is already started.
+  */
   applyErrorHandler(fn: ErrorHandler<T>) {
     if (this.state === ChowChowState.running) {
       throw new Error('Cannot add error handlers once running')
@@ -94,44 +128,49 @@ export class ChowChow<T extends BaseContext = BaseContext> {
     this.errorHandlers.push(fn)
   }
 
-  private validateModules(logIfVerbose: Logger) {
-    let errors = new Array<string>()
+  /**
+    Start up chowchow, calling module callbacks, applying routes
+    and error handlers then starting the express server.
+    It will throw an error if any module's fail #checkEnvironment.
+  */
+  async start({ verbose = false, port = 3000, logErrors = true } = {}) {
+    const logIfVerbose = verbose ? console.log : () => {}
+
+    // Check each module's environment and fail if any of those callbacks throw
+    logIfVerbose('Checking environment')
+    let invalidEnvironment = false
     for (let module of this.modules) {
       try {
         module.checkEnvironment()
         logIfVerbose(' ✓ ' + nameOf(module))
       } catch (error) {
-        errors.push(' 𐄂 ' + nameOf(module) + ': ' + error.message)
+        if (logErrors) {
+          console.log(' ✕ ' + nameOf(module) + ': ' + error.message)
+        }
+        invalidEnvironment = true
       }
     }
 
-    if (errors.length > 0) {
-      errors.forEach(err => console.log(err))
-      process.exit(1)
-    }
-  }
+    if (invalidEnvironment) throw new Error('Invalid environment')
 
-  async start({ verbose = false, port = 3000 }) {
-    const logIfVerbose = verbose ? console.log : () => {}
-
-    logIfVerbose('Checking environment')
-    this.validateModules(logIfVerbose)
-
+    // Setup each module
     logIfVerbose('Setting up modules')
     for (let module of this.modules) {
       await module.setupModule()
       logIfVerbose(' ✓ ' + nameOf(module))
     }
 
+    // Let each module extend express
     logIfVerbose('Extending express')
     for (let module of this.modules) {
-      module.extendExpress(this.server)
+      module.extendExpress(this.expressApp)
       logIfVerbose(' ✓ ' + nameOf(module))
     }
 
+    // Apply routes using their generators
     logIfVerbose('Adding routes')
     for (let fn of this.routesToApply) {
-      fn(this.server, route => async (req, res, next) => {
+      fn(this.expressApp, route => async (req, res, next) => {
         try {
           await route(this.makeCtx(req, res, next))
         } catch (error) {
@@ -141,13 +180,45 @@ export class ChowChow<T extends BaseContext = BaseContext> {
     }
     this.routesToApply = []
 
+    // Apply error handlers using their generators
     logIfVerbose('Adding error handler')
-    this.server.use(((err, req, res, next) => {
+    this.expressApp.use(((err, req, res, next) => {
       let ctx = this.makeCtx(req, res, next)
       for (let handler of this.errorHandlers) handler(err, ctx)
     }) as ExpressHandler)
 
-    await new Promise(resolve => this.server.listen(port, resolve))
+    // Startup the http server
+    await this.startServer(port)
     this.state = ChowChowState.running
+  }
+
+  /** Stop chowchow, clearing modules, stopping the server and resetting it. */
+  async stop() {
+    if (this.state !== ChowChowState.running) return
+
+    // Clear each module in reverse order
+    for (let module of this.modules.reverse()) {
+      module.clearModule()
+    }
+
+    // Stop the http server
+    await this.stopServer()
+    this.state = ChowChowState.stopped
+
+    // Reset the app & server to avoid strange configuration
+    this.expressApp = express()
+    this.httpServer = stoppable(createServer(this.expressApp))
+  }
+
+  /** Internal method to start the express server (overriden in tests) */
+  protected startServer(port: number): Promise<void> {
+    return new Promise(resolve => this.httpServer.listen(port, resolve))
+  }
+
+  /** Internal method to stop the express server (overriden in tests) */
+  protected stopServer(): Promise<void> {
+    return new Promise((resolve, reject) =>
+      this.httpServer.stop(err => (err ? reject(err) : resolve()))
+    )
   }
 }
--- src/__spec__/ChowChow.spec.ts
+++ src/__spec__/ChowChow.spec.ts
@@ -1,5 +1,244 @@
+import {
+  ChowChow,
+  Module,
+  ChowChowInternals,
+  BaseContext,
+  ChowChowState
+} from '../ChowChow'
+import supertest from 'supertest'
+
+class MockChowChow extends ChowChow {
+  startSpy = jest.fn()
+  stopSpy = jest.fn()
+
+  protected startServer(port: number): Promise<void> {
+    this.startSpy(port)
+    return super.startServer(port)
+  }
+
+  protected stopServer(): Promise<void> {
+    this.stopSpy()
+    return super.stopServer()
+  }
+}
+
+class FakeModule implements Module {
+  app: ChowChow = {} as any
+
+  checkEnvironment() {}
+  setupModule() {}
+  clearModule() {}
+  extendExpress() {}
+  extendEndpointContext() {
+    return {}
+  }
+}
+
+type CtxModuleCtx = { name: string }
+class CtxModule extends FakeModule {
+  extendEndpointContext(): CtxModuleCtx {
+    return { name: 'geoff' }
+  }
+}
+
+class BadEnvModule extends FakeModule {
+  checkEnvironment() {
+    throw new Error('Bad Env')
+  }
+}
+
+class SpyModule extends FakeModule {
+  setupSpy = jest.fn()
+  clearSpy = jest.fn()
+  expressSpy = jest.fn()
+  contextSpy = jest.fn()
+
+  setupModule(...args: any[]) {
+    this.setupSpy(...args)
+  }
+  clearModule(...args: any[]) {
+    this.clearSpy(...args)
+  }
+  extendExpress(...args: any[]) {
+    this.expressSpy(...args)
+  }
+  extendEndpointContext(...args: any[]) {
+    this.contextSpy(...args)
+    return {}
+  }
+}
+
 describe('ChowChow', () => {
-  it('should pass', () => {
-    expect(1 + 1).toBe(2)
+  let chow: MockChowChow & ChowChowInternals
+
+  beforeEach(async () => {
+    chow = new MockChowChow() as any
+  })
+
+  afterEach(async () => {
+    await chow.stop()
+  })
+
+  describe('::create', () => {
+    it('should create an instance', async () => {
+      expect(ChowChow.create()).toBeDefined()
+    })
+  })
+
+  describe('#use', () => {
+    it('should register the module', async () => {
+      chow.use(new FakeModule())
+      expect(chow.modules).toHaveLength(1)
+    })
+    it('should put chowchow onto the module', async () => {
+      let module = new FakeModule()
+      chow.use(module)
+      expect(module.app).toBe(chow)
+    })
+  })
+
+  describe('#applyMiddleware', () => {})
+
+  describe('#makeCtx', () => {
+    it('should have the req, res and next', async () => {
+      let req: any = {}
+      let res: any = {}
+      let next: any = () => {}
+      let ctx = chow.makeCtx(req, res, next)
+      expect(ctx.req).toBe(req)
+      expect(ctx.res).toBe(res)
+      expect(ctx.next).toBe(next)
+    })
+    it('should use modules to generate a context', async () => {
+      chow.use(new CtxModule())
+      let ctx = chow.makeCtx({} as any, {} as any, () => {})
+      expect(ctx.name).toBe('geoff')
+    })
+  })
+
+  describe('#applyRoutes', () => {
+    it('should store route generator', async () => {
+      chow.applyRoutes(jest.fn())
+      expect(chow.routesToApply).toHaveLength(1)
+    })
+    it('should not call the generator', async () => {
+      let spy = jest.fn()
+      chow.applyRoutes(spy)
+      expect(spy.mock.calls).toHaveLength(0)
+    })
+    it('should throw an error if started', async () => {
+      await chow.start()
+
+      const applyingRoutes = () => chow.applyRoutes(() => {})
+      expect(applyingRoutes).toThrow()
+    })
+  })
+
+  describe('#applyErrorHandler', () => {
+    it('should store the generator', async () => {
+      chow.applyErrorHandler(jest.fn())
+      expect(chow.errorHandlers).toHaveLength(1)
+    })
+    it('should not call the generator', async () => {
+      let spy = jest.fn()
+      chow.applyErrorHandler(spy)
+      expect(spy.mock.calls).toHaveLength(0)
+    })
+  })
+
+  describe('#start', () => {
+    let spy: SpyModule
+    beforeEach(async () => {
+      spy = new SpyModule()
+      chow.use(spy)
+    })
+
+    it('should start express', async () => {
+      await chow.start()
+      expect(chow.startSpy.mock.calls).toHaveLength(1)
+    })
+    it('should fail for bad environments', async () => {
+      chow.use(new BadEnvModule())
+      const startingUp = chow.start({ logErrors: false })
+      await expect(startingUp).rejects.toThrow()
+    })
+    it('should call #setupModule on modules', async () => {
+      await chow.start()
+      expect(spy.setupSpy.mock.calls).toHaveLength(1)
+    })
+    it('should call #extendExpress on modules', async () => {
+      await chow.start()
+      expect(spy.expressSpy.mock.calls).toHaveLength(1)
+    })
+    it('should apply routes to express', async () => {
+      let route = jest.fn(({ res }: BaseContext) => res.send('hey'))
+
+      chow.applyRoutes((app, r) => {
+        app.get('/', r(route))
+      })
+      await chow.start()
+
+      let agent = supertest(chow.expressApp)
+      let res: supertest.Response = await agent.get('/')
+
+      expect(res.status).toBe(200)
+      expect(route.mock.calls).toHaveLength(1)
+    })
+    it('should register error handlers', async () => {
+      let route = jest.fn(() => {
+        throw new Error('Something went wrong')
+      })
+
+      let errorHandler = jest.fn((err, ctx: BaseContext) => ctx.res.send())
+      chow.applyErrorHandler(errorHandler)
+
+      chow.applyRoutes((app, r) => {
+        app.get('/', r(route))
+      })
+
+      chow.use(new CtxModule())
+
+      await chow.start()
+
+      let agent = supertest(chow.expressApp)
+      await agent.get('/')
+
+      expect(errorHandler.mock.calls).toHaveLength(1)
+      expect(errorHandler.mock.calls[0][0]).toBeInstanceOf(Error)
+      expect(errorHandler.mock.calls[0][1].name).toBe('geoff')
+    })
+    it('should update the state', async () => {
+      await chow.start()
+      expect(chow.state).toBe(ChowChowState.running)
+    })
+  })
+
+  describe('#stop', () => {
+    let spyModule: SpyModule
+    beforeEach(async () => {
+      spyModule = new SpyModule()
+      chow.use(spyModule)
+      await chow.start()
+    })
+    it('should close express', async () => {
+      await chow.stop()
+      expect(chow.stopSpy.mock.calls).toHaveLength(1)
+    })
+    it('should call #clearModule on modules', async () => {
+      await chow.stop()
+      expect(spyModule.clearSpy.mock.calls).toHaveLength(1)
+    })
+    it('should update the state', async () => {
+      await chow.stop()
+      expect(chow.state).toBe(ChowChowState.stopped)
+    })
+    it('should reset the server', async () => {
+      let before = [chow.httpServer, chow.expressApp]
+      await chow.stop()
+      let after = [chow.httpServer, chow.expressApp]
+
+      expect(after[0]).not.toBe(before[0])
+      expect(after[1]).not.toBe(before[1])
+    })
   })
 })
